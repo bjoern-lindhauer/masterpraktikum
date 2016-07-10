d35064e68a33ef9cb1e0890bb0c44c5036ceb193
Robert Rauter <robert.rauter@tu-dortmund.de>
2016-07-10 15:46:02 +0200

    Auswertungsskript weitergeschrieben

--- V21/Daten/auswertung.py
+++ V21/Daten/auswertung.py
@@ -7,6 +7,7 @@ Created on Sun Jul  5 15:07:54 2015
 
 import numpy as np
 from scipy.optimize import curve_fit
+from scipy import constants
 import matplotlib.pyplot as plt
 from uncertainties import ufloat
 import uncertainties.unumpy as unp
@@ -16,61 +17,65 @@ import sympy
 plt.rcParams['figure.figsize'] = (10, 8)
 plt.rcParams['font.size'] = 16
 
-#Daten auslesen
-
-daten1 = np.genfromtxt('landebestimmung.txt', unpack='True')
-daten2 = np.genfromtxt('periodenauswertung.txt', unpack='True')
-
-#Fehlerformelausgabe
-
-def error(f, err_vars=None):
-    from sympy import Symbol, latex
-    s = 0
-    latex_names = dict()
-
-    if err_vars == None:
-        err_vars = f.free_symbols
-
-    for v in err_vars:
-        err = Symbol('latex_std_' + v.name)
-        s += f.diff(v)**2 * err**2
-        latex_names[err] = '\\sigma_{' + latex(v) + '}'
-
-    return latex(sympy.sqrt(s), symbol_names=latex_names)
-
 #Funktionen definieren
 
 def f(x,m, b):
     return m*x+b
 
-#Funktion fitten
+def magnhelm(I, N, R):
+    return constants.mu_0*(8*I*N)/(np.sqrt(125)*R)
 
-guess = [1, 1]
-x_plot=np.linspace(0,10, num=1000)
-params, covariance = curve_fit(f, unp.nominal_values(data1), unp.nominal_values(data2), p0=guess)
+#Daten auslesen
+
+daten1 = np.genfromtxt('landebestimmung.txt', unpack='True')
+daten2 = np.genfromtxt('periodenauswertung.txt', unpack='True')
+
+#Daten transformieren
+
+B1 = magnhelm(daten1[1]*0.1, 11, 0.1639) + magnhelm(daten1[3]*0.3, 154, 0.1579)
+B2 = magnhelm(daten1[2]*0.1, 11, 0.1639) + magnhelm(daten1[4]*0.3, 154, 0.1579)
+
+#Erste Magnetfeld-Messung fitten
+
+x_plot=np.linspace(100,1000, num=10000)
+params, covariance = curve_fit(f,daten1[0], B1)
 
 #Daten und Fit plotten
 
 plt.figure()
-err1 = unp.std_devs(data1)
-err2 = unp.std_devs(data2)
 
-
-plt.errorbar(unp.nominal_values(data1) + err1, -unp.nominal_values(data2) + err2, fmt='bx', label="Messung")
-plt.plot(x_plot, f(x_plot, *params), 'r-', label='Fit')
+plt.plot(daten1[0], B1, 'bx', label="Messung erstes Isotop")
+plt.plot(x_plot, f(x_plot, *params), 'r-', label='linearer Fit')
 plt.legend(loc="best", numpoints=1)
-plt.xlim(0,10)
-plt.ylim(0,10)
-plt.xlabel(r'')
-plt.ylabel(r'')
-plt.savefig('plot1.png')
+plt.xlim(100,1010)
+#plt.ylim(0,6)
+plt.xlabel(r'Frequenz $\nu$ [kHz]')
+plt.ylabel(r'Magnetfeld B [T]')
+plt.savefig('plot1a.pdf')
 
 errors=np.sqrt(np.diag(covariance))
 
 print('m =', params[0], '+/-', errors[0])
 print('b =', params[1], '+/-', errors[1])
 
+# Zweite Magnetfeld-Messung fitten
 
-#x1,x2, x3 = sympy.var('M_{z} M_{0} \tau')
-#f = -x3/(sympy.log((x1-x2)/(2*x2)))
-#print(error(f))
+params, covariance = curve_fit(f,daten1[0], B2)
+
+#Daten und Fit plotten
+
+plt.figure()
+
+plt.plot(daten1[0], B2, 'bx', label="Messung zweites Isotop")
+plt.plot(x_plot, f(x_plot, *params), 'r-', label='linearer Fit')
+plt.legend(loc="best", numpoints=1)
+plt.xlim(100,1010)
+#plt.ylim(0,6)
+plt.xlabel(r'Frequenz $\nu$ [kHz]')
+plt.ylabel(r'Magnetfeld B [T]')
+plt.savefig('plot1b.pdf')
+
+errors=np.sqrt(np.diag(covariance))
+
+print('m =', params[0], '+/-', errors[0])
+print('b =', params[1], '+/-', errors[1])
