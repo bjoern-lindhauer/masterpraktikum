d9b216fcb20ef3137cf87b162539fd5ea620f646
RRRauter <robert.rauter@tu-dortmund.de>
2017-03-13 15:22:29 +0100

    kleinere Fehler beseitigt

--- RReflektrometrie/Daten/auswertung.py
+++ RReflektrometrie/Daten/auswertung.py
@@ -32,9 +32,9 @@ datatxt.close()
 
 l=1.54*10**(-10) #wavelength in Angstrom
 k=2*np.pi/l #wavenumber
-
 ai = np.linspace(0,2.5,10000) #incident angle
-qz = 4*np.pi/(l*10**(10))*np.sin(ai) #change of wave vector
+qz = 4*np.pi/(l*10**(10))*np.sin(np.deg2rad(ai)) #change of wave vector
+n1 = 1 #n(air)
 
 #Peak-Detection
 
@@ -45,27 +45,27 @@ peaks = np.ones((2,26))
 for i in range (1,26):
 
      m = (i-1)*10
-     k = m+10
-     index = np.argmax(data_schwing_mod[1,m:k])
+     n = m+10
+     index = np.argmax(data_schwing_mod[1,m:n])
      index=index+m
      peaks[0,i] = data_schwing_mod[0,index]
      peaks[1,i] = data_schwing_mod[1,index]
 
 peaks = np.delete(peaks, 0, 1)
+peaks = np.delete(peaks, 16, 1)
+dist = np.ones(23)
 
-dist = np.ones(24)
-
-for i in range (1,24):
+for i in range (1,23):
 
     dist[i]=peaks[0,i+1]-peaks[0,i]
 
 dist = np.delete(dist, [0,15], 0)
 
-z2 = l/(2*np.mean(dist))
+z2 = l/(2*np.mean(np.deg2rad(dist)))
 
 print(z2)
 
-def reflectrometry(a,n1,n2,n3,s1,s2,a0):
+def reflectrometry(a,n2,n3,s1,s2,a0):
 
     kz1 =k*scimath.sqrt(n1**2-(np.cos(a))**2)
     kz2 =k*scimath.sqrt(n2**2-(np.cos(a))**2)
@@ -73,25 +73,26 @@ def reflectrometry(a,n1,n2,n3,s1,s2,a0):
 
     r12 = (kz1-kz2)/(kz1+kz2)*np.exp(-2*kz1*kz2*s1**2)
     r23 = (kz2-kz3)/(kz2+kz3)*np.exp(-2*kz2*kz3*s2**2)
-
     x2 = np.exp(-2*1j*kz2*z2)*r23
     x1 = (r12+x2)/(1+r12*x2)
-
     rr = a0*(np.absolute(x1))**2
     return rr
 
 
-x0 = [1, 1-25*10**(-7), 1-75*10**(-7), 55*10**(-11), 35*10**(-11), 2*10**7]
+x0 = [1-20*10**(-7), 1-75*10**(-7), 55*10**(-11), 35*10**(-11), 3*10**7]
 
-#params, covariance = curve_fit(reflectrometry, data[0,:], data[1,:], p0=x0)
+params, covariance = curve_fit(reflectrometry, np.deg2rad(data[0,0:300]), data[1,0:300], p0=x0)
+errors = np.sqrt(np.diag(covariance))
 
+for i in range (0,len(params)):
+    print(params[i], "\t", "+/-", errors[i], "\n")
 
 
 plt.figure()
 plt.plot(data[0], data[1], 'b-', label="Messwerte")
 plt.plot(peaks[0], peaks[1], 'rx', label="verwendete Peaks")
-#plt.plot(ai, reflectrometry(ai, *params), 'y-', label="Fit")
-plt.plot(ai, reflectrometry(ai, *x0), 'g-', label="Test")
+#plt.plot(ai, reflectrometry(np.deg2rad(ai), *params), 'y-', label="Fit")
+plt.plot(ai, reflectrometry(np.deg2rad(ai), *x0), 'g-', label="Test")
 plt.yscale("log")
 plt.legend(loc="best", numpoints=1)
 plt.grid()
--- RReflektrometrie/Daten/skript_auswertung.m
+++ RReflektrometrie/Daten/skript_auswertung.m
@@ -2,15 +2,15 @@
 clear all 
 %Brechungsindex 
 n1=1; %Luft 
-n2=1-25e-7; %Schicht 
-n3=1-75e-7; %Substrat 
+n2=1-25e-8; %Schicht 
+n3=1-75e-8; %Substrat 
 %%%%%%%%%%%%%%%%%%%%%% 
 %Rauigkeit 
 sigma1=55e-11; %Schicht 
 sigma2=35e-11; %Substrat 
 %%%%%%%%%%%%%%%%%%%%%%%% 
 %Schichtdicke 
-z2=2135e-11; 
+z2=147e-10; 
 %Einfallswinkel 
 ai =(0:0.0005:3)*pi /180; 
 %Wellenvektoruebertrag 
@@ -28,9 +28,12 @@ r23=(kz2-kz3 )./( kz2+kz3 ).*exp( -2* kz2 .*kz3*sigma2^2) ;
 x2=exp(-2*i*kz2*z2).*r23 ; 
 x1=(r12+x2)./(1+ r12.*x2) ; 
 
-semilogy (qz , 2e7*abs (x1) .^2 , ' Color ' , ' blue ' ) ; 
-xlabel ( 'q_z [A^{ − 1}] ' ) ; 
-%xlabel ( '\ alpha_i ' ) ; 
+semilogy (qz , 2e7*abs (x1) .^2 , '1' ) ; 
+%xlabel ( 'q_z [A^{ − 1}] ' ) ; 
+xlabel ( '\ alpha_i ' ) ;
+data1 = load('messung_bereinigt.txt' ); 
+hold on; 
+semilogy ( data1 (: ,1) , data1 (: ,2) , '4' ) ; 
 ylabel ( ' intensity ' ); 
-legend ( ' Theorie ');
+legend ( ' Theorie ', 'Messwerte');
  %%%%%%%%%%%%%%%%%%
