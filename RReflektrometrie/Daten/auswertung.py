9cd9a894b6c814f1989059efdb68a90ba527cdf8
RRRauter <robert.rauter@tu-dortmund.de>
2017-03-16 09:07:46 +0100

    Auswertung begonnen zu schreiben

--- RReflektrometrie/Daten/auswertung.py
+++ RReflektrometrie/Daten/auswertung.py
@@ -29,12 +29,24 @@ for i in range (0,len(data[0,:])):
 
 datatxt.close()
 
+plt.figure()
+plt.plot(data_signal[0], data_signal[1], 'r-', label="Messdaten")
+plt.plot(data_noise[0], data_noise[1], 'g-', label="diffuser Scan")
+plt.plot(data[0], data[1], 'b-', label="bereinigte Messdaten")
+plt.yscale("log")
+plt.xlabel(r"$\alpha_{i} / ^{\circ}$")
+plt.ylabel("Intensität / a.u.")
+plt.legend(loc="best", numpoints=1)
+plt.grid()
+plt.savefig("../Protokoll/images/rawdata.pdf")
+
 
 l=1.54*10**(-10) #wavelength in Angstrom
 k=2*np.pi/l #wavenumber
 ai = np.linspace(0,2.5,10000) #incident angle
 qz = 4*np.pi/(l*10**(10))*np.sin(np.deg2rad(ai)) #change of wave vector
 n1 = 1 #n(air)
+ag = 0.3247 #geometry-angle
 
 #Peak-Detection
 
@@ -46,13 +58,13 @@ for i in range (1,26):
 
      m = (i-1)*10
      n = m+10
-     index = np.argmax(data_schwing_mod[1,m:n])
+     index = np.argmin(data_schwing_mod[1,m:n])
      index=index+m
      peaks[0,i] = data_schwing_mod[0,index]
      peaks[1,i] = data_schwing_mod[1,index]
 
 peaks = np.delete(peaks, 0, 1)
-peaks = np.delete(peaks, 16, 1)
+peaks = np.delete(peaks, 10, 1)
 dist = np.ones(23)
 
 for i in range (1,23):
@@ -65,6 +77,20 @@ z2 = l/(2*np.mean(np.deg2rad(dist)))
 
 print(z2)
 print(dist)
+
+def geometryfactor(I,a):
+
+    if (a<=ag):
+        G=np.sin(np.deg2rad(a))/np.sin(np.deg2rad(ag))
+    else:
+        G=1
+
+    return G*I
+
+for i in range (0,len(data[0])):
+    data[1,i]=geometryfactor(data[1,i],data[0,i])
+
+
 def reflectrometry(a,n2,n3,s1,s2):
 
     a0=5*10**7
@@ -75,15 +101,15 @@ def reflectrometry(a,n2,n3,s1,s2):
 
     r12 = (kz1-kz2)/(kz1+kz2)*np.exp(-2*kz1*kz2*s1**2)
     r23 = (kz2-kz3)/(kz2+kz3)*np.exp(-2*kz2*kz3*s2**2)
-    x2 = np.exp(-2*1j*kz2*0.93*z2)*r23
+    x2 = np.exp(-2*1j*kz2*0.96*z2)*r23
     x1 = (r12+x2)/(1+r12*x2)
     rr = a0*(np.absolute(x1))**2
     return rr
 
 
-x0 = [1-20*10**(-7), 1-76*10**(-7), 55*10**(-11), 35*10**(-11)]
+x0 = [1-25*10**(-7), 1-70*10**(-7), 75*10**(-11), 35*10**(-11)]
 
-params, covariance = curve_fit(reflectrometry, np.deg2rad(data[0,0:300]), data[1,0:300], p0=x0)
+params, covariance = curve_fit(reflectrometry, np.deg2rad(data[0,40:300]), data[1,40:300], p0=x0)
 errors = np.sqrt(np.diag(covariance))
 
 for i in range (0,len(params)):
@@ -92,10 +118,10 @@ for i in range (0,len(params)):
 
 plt.figure()
 plt.plot(data[0], data[1], 'b-', label="Messwerte")
-plt.plot(peaks[0], peaks[1], 'rx', label="verwendete Peaks")
+plt.plot(peaks[0], peaks[1], 'rx', label="verwendete Minima")
 plt.plot(ai, reflectrometry(np.deg2rad(ai), *params), 'y-', label="Fit")
 #plt.plot(ai, reflectrometry(np.deg2rad(ai), *x0), 'g-', label="Test")
 plt.yscale("log")
 plt.legend(loc="best", numpoints=1)
 plt.grid()
-plt.savefig("reflectrometry.pdf")
+plt.savefig("../Protokoll/images/reflectrometry.pdf")
