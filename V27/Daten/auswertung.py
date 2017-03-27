2a86fb7ea8ed8c32313bfc3dfe7410ec93ab0554
RRRauter <robert.rauter@tu-dortmund.de>
2017-03-27 15:39:52 +0200

    Daten aus Bildern abgelesen

--- V27/Daten/auswertung.py
+++ V27/Daten/auswertung.py
@@ -15,12 +15,15 @@ import cmath
 plt.rcParams['figure.figsize'] = (10, 8)
 plt.rcParams['font.size'] = 16
 
-data = np.genfromtxt('magnetfeld.txt', unpack='True')
+data_b = np.genfromtxt('magnetfeld.txt', unpack='True')
+
+data_deltas_red = np.genfromtxt('deltas_red.txt', unpack='True', skip_header=1)
+data_deltas_blue = np.genfromtxt('deltas_blue.txt', unpack='True', skip_header=1)
 
 def f(x,a,b):
     return a*x+b
 
-params, covariance = curve_fit(f,data[0],data[1])
+params, covariance = curve_fit(f,data_b[0],data_b[1])
 errors = np.sqrt(np.diag(covariance))
 
 params_err = unp.uarray(params,errors)
@@ -30,7 +33,7 @@ print('b= {:.2u}'.format(params_err[1]))
 x_plot = np.linspace(0,20, 1000)
 
 plt.figure()
-plt.plot(data[0], data[1], 'bx', label="gemessenes Magnetfeld")
+plt.plot(data_b[0], data_b[1], 'bx', label="gemessenes Magnetfeld")
 plt.plot(x_plot, f(x_plot, *params), 'r-', label="linearer Fit")
 plt.xlabel(r"Stromstärke I / A")
 plt.ylabel(r"Magnetfeld B / T")
