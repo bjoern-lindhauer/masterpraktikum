506cacab0e49f76521c29e64f00ab2d5f6a023ac
RRRauter <robert.rauter@tu-dortmund.de>
2017-03-28 16:18:06 +0200

    Wellenlaengenverschiebung fertig

--- V27/Daten/auswertung.py
+++ V27/Daten/auswertung.py
@@ -5,6 +5,7 @@ from numpy.lib import scimath
 import scipy
 from scipy.optimize import curve_fit
 from scipy import constants
+from scipy.stats import sem
 import matplotlib.pyplot as plt
 from uncertainties import ufloat
 import uncertainties.unumpy as unp
@@ -15,6 +16,14 @@ import cmath
 plt.rcParams['figure.figsize'] = (10, 8)
 plt.rcParams['font.size'] = 16
 
+h=constants.value(u'Planck constant')
+c=constants.value(u'speed of light in vacuum')
+mb = constants.value(u'Bohr magneton')
+l_red = 480*10**(-9)
+l_blue = 643.8*10**(-9)
+ld_red = 48.9*10**(-12)
+ld_blue = 26.9*10**(-12)
+
 data_b = np.genfromtxt('magnetfeld.txt', unpack='True')
 
 data_deltas_red = np.genfromtxt('deltas_red.txt', unpack='True', skip_header=1)
@@ -22,6 +31,12 @@ data_deltas_blue = np.genfromtxt('deltas_blue.txt', unpack='True', skip_header=1
 
 def f(x,a,b):
     return a*x+b
+
+def dlambda(ds, Ds, Dl):
+    return 0.5*ds/Ds*Dl
+
+def lande(l,B,dl):
+    return h*c/(l**2*mb*B)*dl
 
 params, covariance = curve_fit(f,data_b[0],data_b[1])
 errors = np.sqrt(np.diag(covariance))
@@ -40,3 +55,36 @@ plt.ylabel(r"Magnetfeld B / T")
 plt.legend(loc="best", numpoints=1)
 plt.grid()
 plt.savefig("../Protokoll/images/magnetfeld.pdf")
+
+#calculation of change of wave-length
+
+delta_l_red = dlambda(data_deltas_red[1],data_deltas_red[0],ld_red)
+
+delta_l_blue = np.ones((2,10))
+
+delta_l_blue[0] = dlambda(data_deltas_blue[1], data_deltas_blue[0], ld_blue)
+delta_l_blue[1] = dlambda(data_deltas_blue[2], data_deltas_blue[0], ld_blue)
+
+dl_red = ufloat(np.mean(delta_l_red), sem(delta_l_red))
+dl_blue = unp.uarray([np.mean(delta_l_blue[0]),np.mean(delta_l_blue[1])] , [sem(delta_l_blue[0]),sem(delta_l_blue[1])])
+
+print('dl(643.8nm) = {:.2u}'.format(dl_red))
+print('B = {:.5}'.format(f(11.5,*params)))
+print('dl(480.0nm) = {:.2u}'.format(dl_blue[0]))
+print('B = {:.5}'.format(f(5.5,*params)))
+print('dl(480.0nm) = {:.2u}'.format(dl_blue[1]))
+print('B = {:.5}'.format(f(18,*params)))
+
+# for i in range (0,len(delta_l_red)):
+#     print(i, "\t", '{:.4}'.format(delta_l_red[i]), "\n")
+#
+# for i in range (0,len(delta_l_blue[0])):
+#     print(i, "\t", '{:.4}'.format(delta_l_blue[0,i]), "\n")
+#
+# for i in range (0,len(delta_l_blue[1])):
+#     print(i, "\t", '{:.4}'.format(delta_l_blue[1,i]), "\n")
+
+print('Die Landé-Faktoren lauten:')
+print('\t', 'rot:', lande(l_red,f(11.5,*params),dl_red))
+print('\t', 'blau, sigma:', lande(l_blue,f(5.5,*params),dl_blue[0]))
+print('\t', 'blau, pi:', lande(l_blue,f(18,*params),dl_blue[1]))
